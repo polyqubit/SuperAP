2d162b9188c021dc8df995cb2c162144e9dc01ed
polyqubit <polyumbrial@gmail.com>
2023-01-12 14:32:27 -0800

    not fixed

--- lab8/starter.java
+++ lab8/starter.java
@@ -1,55 +1,58 @@
 public class starter {
     public static void pArr(int[] a) {
         System.out.print("{ ");
-        for(int i : a) {
-            System.out.print(i+" ");
+        for (int i : a) {
+            System.out.print(i + " ");
         }
         System.out.println("}");
     }
+
     public static void radix(int[] a) {
         Queue[] qa = new Queue[10];
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             qa[i] = new Queue();
         }
         int[] tq = new int[a.length];
-        for(int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             tq[i] = a[i];
         }
         pArr(tq);
         int maxDigit = 0;
-        for(int i : a) {
-            maxDigit = (Integer.toString(i).length()>maxDigit) ? Integer.toString(i).length() : maxDigit;
+        for (int i : a) {
+            maxDigit = (Integer.toString(i).length() > maxDigit) ? Integer.toString(i).length() : maxDigit;
         }
         int place = 0;
-        while(place <= maxDigit) {
+        while (place < maxDigit) {
             divideTen(qa, tq, place);
             int c = 0;
-            for(Queue q : qa) {
-                System.out.print(q.getSize() + " ");
-                while(!q.isEmpty()) {
+            for (Queue q : qa) {
+                System.out.print(q.getSize() + ":[ ");
+                while (!q.isEmpty()) {
                     tq[c] = q.dequeue();
-                    //System.out.println(tq[c]);
+                    System.out.print(tq[c] + " ");
                     c++;
                 }
+                System.out.print("]  ");
             }
-            System.out.println("end: " + place);
+            System.out.println("end: " + place + " - " + c);
             place++;
         }
-        for(int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             a[i] = tq[i];
         }
     }
+
     private static void divideTen(Queue[] qa, int[] data, int digit) {
-        for(int i : data) {
-            qa[(i/(int)Math.pow(10,digit))%10].enqueue(i);
+        for (int i : data) {
+            qa[(i / (int) Math.pow(10, digit)) % 10].enqueue(i);
         }
     }
 
     public static void main(String args[]) {
-        int[] t = {999112,321,4343,23994};
+        int[] t = { 999112, 321, 4343, 23994 };
         radix(t);
         pArr(t);
     }
 }
 
-// 65432 
+// 65432
